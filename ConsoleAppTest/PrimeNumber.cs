3f24b4b796fad4bdc06f557e5346ea0672446fe3
unknown <erick.a.a.nery@gmail.com>
2024-06-23 19:46:35 -0300

    removendo todos

--- ConsoleAppTest/PrimeNumber.cs
+++ ConsoleAppTest/PrimeNumber.cs
@@ -1,85 +1,108 @@
-using System.Diagnostics;
-using System.Net.NetworkInformation;
+namespace Erick.Prime
+{
 
-namespace Erick.Prime{
+    public class PrimeNumber
+    {
 
-  public class PrimeNumber{
-
-    public static int PrimeNumberQuantityGet(long number){
-        if(number < 2) return 0;
-        var PrimeNumberQtd = 0;
-        
-        for (int i = 2; i <= number; i++)
+        public static int PrimeNumberQuantityGet(long number)
         {
-            if(IsPrime(i)) {
-                PrimeNumberQtd++;
+            if (number < 2) return 0;
+            var PrimeNumberQtd = 0;
+
+            for (int i = 2; i <= number; i++)
+            {
+                if (IsPrime(i))
+                {
+                    PrimeNumberQtd++;
+                }
             }
+
+            return PrimeNumberQtd;
         }
 
-        return PrimeNumberQtd;
-    }
-
-    public async static Task<int> PrimeNumberQuantityGetAssync(long number){
-
-        if(number < 2) return 0;
-        Int64 remainder = 0;
-        var numberFractionated = Math.DivRem(Int64.Parse(number.ToString()), 4,out remainder);
-
-        //Console.WriteLine(numberFractionated-1);
-        //Console.WriteLine(numberFractionated*2-1);
-        //Console.WriteLine(numberFractionated*3-1);
-        //Console.WriteLine(numberFractionated*4);
-
-
-        var primeNumbersList = new List<Task<int>>
+        public async static Task<int> PrimeNumberQuantityGetAsync(long number)
         {
-            Task.Run(() => PrimeNumberQuantityGetAssyncRange(2, numberFractionated - 1)),
-            Task.Run(() => PrimeNumberQuantityGetAssyncRange(numberFractionated/2, numberFractionated*2-1)),
-            Task.Run(() => PrimeNumberQuantityGetAssyncRange(numberFractionated*2, numberFractionated*3-1)),
-            Task.Run(() => PrimeNumberQuantityGetAssyncRange(numberFractionated*2, numberFractionated*4)),
-            Task.Run(() => PrimeNumberQuantityGetAssyncRange(numberFractionated*4, numberFractionated*4+remainder))
+            if (number < 2) return 0;
+            long remainder = 0;
+            var numberFractionated = Math.DivRem(Int64.Parse(number.ToString()), 4, out remainder);
+
+            int counter = 1;
+
+            var primeNumbersList = new List<Task<int>>
+        {
+            PrimeNumberQuantityGetAssyncRange(2, numberFractionated*counter - 1),
+            PrimeNumberQuantityGetAssyncRange(numberFractionated*counter, numberFractionated*++counter-1),
+            PrimeNumberQuantityGetAssyncRange(numberFractionated*counter, numberFractionated*++counter-1),
+            PrimeNumberQuantityGetAssyncRange(numberFractionated*counter, numberFractionated*++counter-1),
+            PrimeNumberQuantityGetAssyncRange(numberFractionated*counter, numberFractionated*counter+remainder)
         };
 
-        int[] resultList = await Task.WhenAll(primeNumbersList);
-        return resultList.Sum();
-    }
-
-    public static int PrimeNumberQuantityGetAssyncRange(long start,long limit){
-        int counter = 0; 
-        for (long i = start; i <= limit; i++)
-        {
-            if(IsPrime(i)) counter++;
+            int[] resultList = await Task.WhenAll(primeNumbersList);
+            return resultList.Sum();
         }
-        return counter;
-    }
 
-    private static async Task IsPrimeAssync(long number, int counter){
-        if(IsPrime(number)) counter++;
-        return;
-    }
-
-    public static int PrimeNumberQuantityGetParallel(long number){
-        if(number < 2) return 0;
-        var PrimeNumberQtd = 0;
-
-        Parallel.For(2, number, i =>
+        public static Task<int> PrimeNumberQuantityGetAssyncRange(long start, long limit)
         {
-            if(IsPrime(i)) {
-                PrimeNumberQtd++;
-            }            
-        });
-        return PrimeNumberQtd;
-    }
+            return Task.Run<int>(async () =>
+            {
 
-    private static bool IsPrime(long number)
-    {
-        if(number == 2) return true;
-        for (int i = 2; i < number; i++)
-        {
-            if (number%i == 0)
-            return false;
+                List<Task<int>> listaNumeros = new List<Task<int>>();
+
+                for (long i = start; i <= limit; i++)
+                {
+                    listaNumeros.Add(IsPrimeAssync(i));
+                }
+
+                var listaints = await Task.WhenAll(listaNumeros);
+                return listaints.Sum();
+
+            });
         }
-        return true;
+
+        private static Task<int> IsPrimeAssync(long number)
+        {
+            return Task.Run<int>(() =>
+            {
+                if (number == 2)
+                {
+                    return 1;
+                }
+                for (int i = 2; i < number; i++)
+                {
+                    if (number % i == 0)
+                        return 0;
+                }
+                return 1;
+            });
+        }
+
+        public static int PrimeNumberQuantityGetParallel(long number)
+        {
+            if (number < 2) return 0;
+            var PrimeNumberQtd = 0;
+
+            Parallel.For(2, number, i =>
+            {
+                if (IsPrime(i))
+                {
+                    PrimeNumberQtd++;
+                }
+            });
+            return PrimeNumberQtd;
+        }
+
+        private static bool IsPrime(long number)
+        {
+            if (number == 2)
+            {
+                return true;
+            }
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+            return true;
+        }
     }
-  }
 }
--- ConsoleAppTest/Program.cs
+++ ConsoleAppTest/Program.cs
@@ -1,56 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Diagnostics;
-using System.Runtime.InteropServices;
+﻿using System.Diagnostics;
 using Erick.Prime;
-
-//TODO Ver se a escrita esta certa, acertar nome de variavel e metodo
-
-public class Program{
-
-static async Task Main()
+public class Program
 {
 
-    Console.WriteLine("Initializing...");
-    Stopwatch stopWatch = new Stopwatch();
-    stopWatch.Start();
-    long number = 1000000; int result = 0; int option;
-
-    Console.WriteLine("Choose method to process");
-    Console.WriteLine(" 1 - Syncronous task");
-    Console.WriteLine(" 2 - Assyncronous task");
-    Console.WriteLine(" 3 - Parallel task");
-
-    option =  int.Parse(Console.ReadLine() ?? "0");
-    
-    /*await Task.Run( async () => 
+    static async Task Main()
     {
-        result = await Task.Run(() => PrimeNumber.PrimeNumberQuantityAsync(number));
-    } );*/
 
-    switch (option)
-    {
-        case  1:
-            Console.WriteLine($"Until {number} there are {PrimeNumber.PrimeNumberQuantityGet(number)}  numbers");
-            break;
-        case  2:
-            result = await PrimeNumber.PrimeNumberQuantityGetAssync(number);
-            Console.WriteLine($"Until {number} there are {result}  numbers");
-            break;
-        case  3:
-            Console.WriteLine($"Until {number} there are {PrimeNumber.PrimeNumberQuantityGetParallel(number)}  numbers");
-            break;
-        default:
-            Console.WriteLine($"Invalid option");
-            break;
-    }
+        Console.WriteLine("Initializing...");
+        Stopwatch stopWatch = new Stopwatch();
+        stopWatch.Start();
 
-    //Console.WriteLine($"Until {number} there are {PrimeNumber.PrimeNumberQuantityGet(number)}  numbers");
-    //Console.WriteLine($"Until {number} there are {result}  numbers");
-    
-    TimeSpan ts = stopWatch.Elapsed;
-    Console.WriteLine($"Total time spent: {ts.ToString()}");
+        long number = 1000000;
+        int option;
+        Console.WriteLine("Choose method to process");
+        Console.WriteLine(" 1 - Syncronous task");
+        Console.WriteLine(" 2 - Assyncronous task");
+        Console.WriteLine(" 3 - Parallel task");
 
+        option = int.Parse(Console.ReadLine() ?? "0");
 
+        switch (option)
+        {
+            case 1:
+                Console.WriteLine($"Until {number} there are {PrimeNumber.PrimeNumberQuantityGet(number)}  numbers");
+                break;
+            case 2:
+                int result = await PrimeNumber.PrimeNumberQuantityGetAsync(number);
+                Console.WriteLine($"Until {number} there are {result}  numbers");
+                break;
+            case 3:
+                Console.WriteLine($"Until {number} there are {PrimeNumber.PrimeNumberQuantityGetParallel(number)}  numbers");
+                break;
+            default:
+                Console.WriteLine($"Invalid option");
+                break;
+        }
+        TimeSpan ts = stopWatch.Elapsed;
+        Console.WriteLine($"Total time spent: {ts.ToString()}");
     }
 
 }
